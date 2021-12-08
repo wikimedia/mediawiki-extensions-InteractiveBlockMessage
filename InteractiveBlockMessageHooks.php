069a2eb8de65bc896de78bcc0fb061d7ec86d7f4
Umherirrender <umherirrender_de.wp@web.de>
2021-12-08 18:48:35 +0100

    Replace deprecated Block::$mExpiry by getter
    The getter exists since 1.19
    
    Change-Id: Ie24bb215f9f00e02a0fc467a6807da27fbba1407

--- InteractiveBlockMessageHooks.php
+++ InteractiveBlockMessageHooks.php
@@ -42,7 +42,7 @@ class InteractiveBlockMessageHooks {
 			// if user is blocked it's pretty much possible they will be unblocked one day :)
 			// so we enable cache for shorter time only so that we can recheck later
 			// if they weren't already unblocked - if there is a better way to do that, fix me
-			$expiry = $user->getBlock()->mExpiry;
+			$expiry = $user->getBlock()->getExpiry();
 			if ( is_numeric( $expiry ) ) {
 				// sometimes this is 'infinityinfinity'. in that case, use the default cache expiry time.
 				$expiry = wfTimestamp( TS_UNIX, $expiry ) - wfTimestamp( TS_UNIX );
